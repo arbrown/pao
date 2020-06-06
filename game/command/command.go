8497515ac6f962b50adcb9cbdb3c4cea2dd643f8
Drew Brown <drew@drewbrown.me>
2020-06-06 12:14:02 -0600

    Refactor command and player into separate packages
    Minor formatting and comment changes in various files

---
+++ game/command/command.go
@@ -0,0 +1,46 @@
+package command
+
+import "github.com/arbrown/pao/game/player"
+
+// Command is a generic command between a pao server/client
+type Command struct {
+	Action, Argument string
+}
+
+// PlayerCommand is a command from a player to the server
+type PlayerCommand struct {
+	C Command
+	P *player.Player
+}
+
+// ChatCommand is a chat message broadcast from server to client
+type ChatCommand struct {
+	Action, Player, Color, Message string
+	Auth                           bool
+}
+
+// BoardCommand is an update of the board state from server to client
+type BoardCommand struct {
+	Action     string
+	Board      [][]string
+	Dead       []string
+	LastMove   []string
+	LastDead   string
+	YourTurn   bool
+	WhoseTurn  string
+	TurnColor  string
+	NumPlayers int
+}
+
+// ColorCommand is a command indicating to a client which color is theirs
+// upon a flip
+type ColorCommand struct {
+	Action, Color string
+}
+
+// GameOverCommand is a command from the server to clients indicating that
+// the game has concluded and who has won
+type GameOverCommand struct {
+	Action, Message string
+	YouWin          bool
+}
---
+++ game/player/player.go
@@ -0,0 +1,24 @@
+package player
+
+import (
+	"github.com/apexskier/httpauth"
+	"github.com/gorilla/websocket"
+)
+
+// NewPlayer will create a new player (or kibitzer) for a pao server
+func NewPlayer(c *websocket.Conn, name string, user *httpauth.UserData, kibitzer bool) *Player {
+	return &Player{
+		Ws:       c,
+		Name:     name,
+		User:     user,
+		Kibitzer: kibitzer,
+	}
+}
+
+// Player contains information about a player and how to communicate with them
+type Player struct {
+	Name     string
+	Ws       *websocket.Conn
+	User     *httpauth.UserData
+	Kibitzer bool
+}
